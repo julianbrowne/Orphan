c64763c47870c9f5c8e5f3038eae93bd23583728
Julian Browne <julianbrowne@gmail.com>
2014-08-03 09:18:14 +0100

    cleaned up tests

---
+++ test/ui.spec.js
@@ -0,0 +1,42 @@
+
+describe('UI', function () { 
+
+    beforeEach(function() { 
+        results = document.createElement('div');
+        results.id = 'test-results';
+        var body = document.getElementsByTagName('body')[0];
+        body.appendChild(results);
+    });
+
+    afterEach(function() { 
+        var results = document.getElementById('test-results');
+        results.parentNode.removeChild(results);
+    });
+
+    it('should add a div', function () { 
+        Orphan.ui.addDiv('bob','test-results');
+        var r1 = document.getElementById('test-results').childNodes[0];
+        expect(r1).not.toBeNull();
+        expect(r1.innerHTML).toEqual('bob');
+    });
+
+    it('should get an element', function () { 
+        var r1 = Orphan.ui.getElement('test-results');
+        console.log(r1);
+        expect(r1).not.toBeNull();
+        if(r1.outerHTML)
+            expect(r1.outerHTML).toEqual('<div id="test-results"></div>');
+    });
+
+    it('should make elements', function () { 
+        var r1 = Orphan.ui.makeElement('a');
+        var r2 = Orphan.ui.makeElement('p');
+        expect(r1).not.toBeNull();
+        if(r1.outerHTML)
+            expect(r1.outerHTML).toEqual('<a></a>');
+        expect(r2).not.toBeNull();
+        if(r2.outerHTML)
+            expect(r2.outerHTML).toEqual('<p></p>');
+    });
+
+});
